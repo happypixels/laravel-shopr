e426d6e97c595a372fad2a04a59dc79fad6449be
Mattias Persson <mattias@happypixels.se>
2018-11-17 16:16:25 +0100

    Debug Klarna bad request

--- src/Controllers/OrderController.php
+++ src/Controllers/OrderController.php
@@ -31,16 +31,13 @@ class OrderController extends Controller
             'cart' => [new CartNotEmpty]
         ]);
 
-        try {
-            $provider = PaymentProviderManager::make($request);
+        // try {
+        $provider = PaymentProviderManager::make($request);
 
-            return $provider->createProviderOrder();
-        } catch (\Exception $e) {
-            return response()->json([
-                'message' => 'We were unable to process your order.',
-                'exception' => json_encode($e)
-            ], 400);
-        }
+        return $provider->createProviderOrder();
+        // } catch (\Exception $e) {
+        //     return response()->json('We were unable to process your order.', 400);
+        // }
     }
 
     /**
